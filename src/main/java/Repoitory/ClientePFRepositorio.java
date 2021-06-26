b040de5c5cc5b5ff51b699dc85b6c2c0c3d9b145
Yuri Dias <yuri.dazevedo@gmail.com>
2021-06-26 17:50:31 -0300

    feat: create ClienteRepository

---
+++ src/main/java/Repoitory/ClientePFRepositorio.java
@@ -0,0 +1,32 @@
+package Repoitory;
+
+import Model.Cliente.ClientePF;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ClientePFRepositorio {
+
+    private List<ClientePF> lista;
+
+    public ClientePFRepositorio(List<ClientePF> lista){
+        this.lista = lista;
+    }
+
+    void salvar(ClientePF cliente){
+        this.lista.add(cliente);
+    }
+
+    Optional<ClientePF> getById(int id){
+        return this.lista
+                .stream()
+                .filter(c -> c.getId() == id)
+                .findFirst();
+
+    }
+
+    void remove(int id){
+        this.lista.removeIf(c -> c.getId() == id);
+    }
+
+}
