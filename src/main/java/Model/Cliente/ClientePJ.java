8cee2ffabd466340da4415bfc8415b738c265a6f
Yuri Dias <yuri.dazevedo@gmail.com>
2021-06-26 22:10:11 -0300

    feat: cnpj validate

--- src/main/java/Model/Cliente/ClientePJ.java
+++ src/main/java/Model/Cliente/ClientePJ.java
@@ -10,6 +10,21 @@ public class ClientePJ extends Cliente {
     private String razaoSocial;
     private String cnpj;
 
+    public ClientePJ setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+        return this;
+    }
+
+    public ClientePJ setCnpj(String cnpj) {
+        if(!cnpj.matches("/^\\d{2}\\.\\d{3}\\.\\d{3}\\/\\d{4}\\-\\d{2}$/")){
+            System.out.println("Cnpj inválido");
+            return this;
+        }
+
+        this.cnpj = cnpj;
+        return this;
+    }
+
     public String getRazaoSocial() {
         return razaoSocial;
     }
