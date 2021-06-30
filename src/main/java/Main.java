2da0b87c0524d826129844230d0e2adcd25263fe
Yuri Dias <yuri.dazevedo@gmail.com>
2021-06-30 14:39:27 -0300

    create: main

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        * Grupo
+        *
+        * Nícolas Moraes
+        * Yuri Dias
+        *
+        * */
+    }
+}
